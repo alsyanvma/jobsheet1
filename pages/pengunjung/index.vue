b719657e7ec720fa5323812d1297bf900c87ec88
alsyaaa <142280309+alsyanvma@users.noreply.github.com>
2024-02-26 16:08:03 +0700

    Add files via upload

---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+                <div class="my-3">
+                    <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <td>#</td>
+                                <td>NAMA</td>
+                                <td>KEANGGOTAAN</td>
+                                <td>WAKTU</td>
+                                <td>KEPERLUAN</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Alsya Novema</td>
+                                <td>Siswa</td>
+                                <td>26 Febuari 2024, 23.31.00</td>
+                                <td>Baca</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
